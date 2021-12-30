8450fbc76a42b1d7e05b2569e03ab43e316a7cbb
AlexShtarbev <alex.shtarbev@gmail.com>
2021-12-30 17:41:04 +0200

    add base of vaadin

---
+++ AtlasArchiverUI/src/main/java/com/ashtarbev/atlas/archiver/ui/Application.java
@@ -0,0 +1,15 @@
+package com.ashtarbev.atlas.archiver.ui;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ AtlasArchiverUI/src/main/java/com/ashtarbev/atlas/archiver/ui/MainView.java
@@ -0,0 +1,12 @@
+package com.ashtarbev.atlas.archiver.ui;
+
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route
+public class MainView extends VerticalLayout {
+    public MainView() {
+        add(new Text("Welcome to MainView."));
+    }
+}
